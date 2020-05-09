7a660d3326e62850f442043079e0e3d334041d6d
Valentina Panic <valentina.panic87@gmail.com>
2020-05-09 18:22:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
-  attendees.each do |name|
-     "Hello, my name is #{name}."
+  attendees.each do |badge|
+     "Hello, my name is #{badge}."
   end
 end
