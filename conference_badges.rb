a2b125e14af3ec8af93dc9701602f1892d345b78
Valentina Panic <valentina.panic87@gmail.com>
2020-05-09 19:55:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,8 @@ def assign_rooms(attendees)
   list
 end
 def printer(attendees)
-   batch_badge_creator(attendees).each do |list|
+   batch_badge_creator(attendees).each do |badge|
+     puts badge
 
    end
 end
